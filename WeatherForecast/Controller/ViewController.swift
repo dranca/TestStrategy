e3d86f2bbf8a9ba5a1e85c9f222d7c543788c8e0
Christopher Batin <chris@batin.me>
2018-06-01 15:40:57 +0100

    Added MVVM structure

--- WeatherForecast/Controller/ViewController.swift
+++ WeatherForecast/Controller/ViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
     private let apiManager = APIManager()
+    private(set) var windViewModel: WindViewModel?
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var coordLabel: UILabel!
+
+    var searchResult: CurrentWeather? {
+        didSet {
+            guard let searchResult = searchResult else { return }
+            windViewModel = WindViewModel.init(currentWeather: searchResult)
+            DispatchQueue.main.async {
+                self.updateLabels()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +45,14 @@ class ViewController: UIViewController {
 
 extension ViewController {
 
+    private func updateLabels() {
+        guard let windViewModel = windViewModel else { return }
+        locationLabel.text = windViewModel.locationString
+        windSpeedLabel.text = windViewModel.windSpeedString
+        windDirectionLabel.text = windViewModel.windDegString
+        coordLabel.text = windViewModel.coordString
+    }
+
     private func getWeather() {
         apiManager.getWeather() { (weather, error) in
             if let error = error {
@@ -37,9 +60,9 @@ extension ViewController {
                 return
             }
             guard let weather = weather  else { return }
+            self.searchResult = weather
             print("Current Weather Object:")
             print(weather)
         }
     }
-
 }
